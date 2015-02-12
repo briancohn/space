c6d918fca7e7607e785ade81096c1741e2d69831
Brian <bcohn12@gmail.com>
2015-02-11 18:12:50 -0800

    all to CamelCase

--- src/main/scala/bbdl/polytope/MyAdder.scala
+++ src/main/scala/bbdl/polytope/MyAdder.scala
@@ -8,69 +8,66 @@ class MyAdder extends Function2[Double, Double, Double] {
 	}
 }
 
-object get_random_direction {
+class GetRandomDirection {
 	def apply(B: DenseMatrix[Double], v: DenseVector[Double]) = {
 		0
 	}
 
 }
-object extract_right_square {
-	def apply(M: DenseMatrix[Double]) = {
-		val row_num = M.rows //m
-		val col_num = M.cols //n
-		val start_col = col_num-row_num
-		var output = M(::, start_col to -1)
-		output
-		}
-}
 
-object insert_col {
-	def apply(M: DenseMatrix[Double], v: DenseVector[Double], k: Int) = {
-		for( row_num <- 0 to M.rows) {
-			M(row_num, k) = v(row_num)
+
+
+
+
+// @return B DenseMatrix[Double] basis vectors matrix, with each column representing a basis vector.
+class Basis{
+	def ExtractRightSquare(M: DenseMatrix[Double]) = {
+		val RowNum = M.rows //m
+		val ColNum = M.cols //n
+		val StartCol = ColNum-RowNum
+		var output = M(::, StartCol to -1)
+		output
+	}
+	def InsertCol(M: DenseMatrix[Double], v: DenseVector[Double], k: Int) = {
+		for( RowNum <- 0 to M.rows) {
+			M(RowNum, k) = v(RowNum)
 		}
 		M
 	}
-	
-}
-
-// @return B DenseMatrix[Double] basis vectors matrix, with each column representing a basis vector.
-class basis{
 	def apply(generators: DenseMatrix[Double]) = {
-		val row_num = generators.rows
-		val col_num = generators.cols
-		var basis_solutions = DenseMatrix.zeros[Double](row_num, col_num-row_num)
-		if (row_num > col_num)
+		val RowNum = generators.rows
+		val ColNum = generators.cols
+		var BasisSolutions = DenseMatrix.zeros[Double](RowNum, ColNum-RowNum)
+		if (RowNum > ColNum)
     		println("You need to input a matrix where the number of columns is equal to or higher than the number of rows.")
-		var current_col_to_insert = 0 
-		val list = List.range(0, col_num-row_num-1) 
+		var CurrentColToInsert = 0 
+		val list = List.range(0, ColNum-RowNum-1) 
 		for(i  <- list) {
-			val right_square = extract_right_square(generators).toDenseMatrix
-			val current_col = generators(::,i).toDenseMatrix
-			val x = right_square \ -current_col
-			val current_solutions = x.toDenseVector(1 to -1) //exclude the first element, which we know is a one.
-			basis_solutions = insert_col(basis_solutions, current_solutions, current_col_to_insert)
-			current_col_to_insert = current_col_to_insert + 1
+			val RightSquare = ExtractRightSquare(generators).toDenseMatrix
+			val CurrentCol = generators(::,i).toDenseMatrix
+			val x = RightSquare \ -CurrentCol
+			val CurrentSolution = x.toDenseVector(1 to -1) //exclude the first element, which we know is a one.
+			BasisSolutions = InsertCol(BasisSolutions, CurrentSolution, CurrentColToInsert)
+			CurrentColToInsert = CurrentColToInsert + 1
 		}
 		//generate a square matrix with ones down the middle, to represent each of the basis vectors we solved for.
-		val diagonal_mat = DenseMatrix.eye[Double](col_num-row_num)
-		DenseMatrix.vertcat(diagonal_mat,basis_solutions)
+		val DiagonalMat = DenseMatrix.eye[Double](ColNum-RowNum)
+		DenseMatrix.vertcat(DiagonalMat,BasisSolutions)
 	}
-
 }
 	
 
 
-class orth {
+class Ortho {
 	def apply(a: DenseMatrix[Double]) = {
 		val m = a.rows
 		val n = a.cols
-		var basis_vector = a(::, 0)
-		var b = DenseMatrix.create[Double](m, 1, (basis_vector / norm(basis_vector)).toArray)
+		var BasisVector = a(::, 0)
+		var b = DenseMatrix.create[Double](m, 1, (BasisVector / norm(BasisVector)).toArray)
 		for (i <- 1 until n) {
-		  basis_vector = a(::, i)
-		  basis_vector = basis_vector - b * (b.t * basis_vector)
-		  b = DenseMatrix.horzcat(b, DenseMatrix.create[Double](m, 1, basis_vector.toArray))
+		  BasisVector = a(::, i)
+		  BasisVector = BasisVector - b * (b.t * BasisVector)
+		  b = DenseMatrix.horzcat(b, DenseMatrix.create[Double](m, 1, BasisVector.toArray))
 		}
 		b
 	} 
--- src/test/scala/bbdl/polytope/MyAdderSpec.scala
+++ src/test/scala/bbdl/polytope/MyAdderSpec.scala
@@ -1,7 +1,8 @@
 package bbdl.polytope
-import org.scalatest._
 import breeze.linalg._
 import breeze.numerics._
+import org.scalatest._ //added for this test file
+
 class MyAdderSpec extends FlatSpec with Matchers {
 	behavior of "MyAdder"
 
@@ -15,64 +16,63 @@ class MyAdderSpec extends FlatSpec with Matchers {
 }
 
 
-class get_random_direction_spec extends FlatSpec with Matchers {
-	behavior of "get_random_direction"
+class GetRandomDirectionSpec extends FlatSpec with Matchers {
+	behavior of "GetRandomDirection"
 	it should "Get a random direction for all positive inputs" in {
-	val get_random_direction = new get_random_direction()
+	val GetRandomDirection = new GetRandomDirection()
 	  val B = DenseMatrix((1.0,0.0), (0.0,1.0), (0.0, 0.0))
 	  val v = DenseVector(0.1,0.2)
-	  val random_direction = get_random_direction(B,v)
+	  val RandomDirection = GetRandomDirection(B,v)
 	  val expected = DenseVector(0.1,0.2,0.0)
-	  random_direction should be expected
+	  RandomDirection should be expected
 	}
 	it should "get a random direction for some negative inputs" in {
 	  val B = DenseMatrix((-1.0,0.0), (0.0,1.0), (0.0, 0.0))
 	  val v = DenseVector(0.1,-0.2)
-	  val random_direction = get_random_direction(B,v)
+	  val RandomDirection = GetRandomDirection(B,v)
 	  val expected = DenseVector(-0.1, -0.2, 0.0)
-	  random_direction should be expected
+	  RandomDirection should be expected
 	}
 }
 
-class basis_spec extends FlatSpec with Matchers {
-	behavior of "generate_basis"
+class BasisSpec extends FlatSpec with Matchers {
+	behavior of "GenerateBasis"
 	it should "take in a matrix of size (4,2) (generators)" in {
 	  val A = DenseMatrix((1.0,1.0,1.0,1.0), (2.0,1.0,1.0,1.0))
-	  val basis = generate_basis(A)
-	  val expected_basis = DenseMatrix((0.0,0.0),(-1.0,-1.0), (1.0,0.0), (0.0, 1.0))
-	  basis should be expected_basis
+	  val basis = GenerateBasis(A)
+	  val ExpectedBasis = DenseMatrix((0.0,0.0),(-1.0,-1.0), (1.0,0.0), (0.0, 1.0))
+	  basis should be ExpectedBasis
 	}
 	it should "take in a matrix of size (2,4) generators" in {
 	  val A = DenseMatrix((0.0,0.0,1.0,1.0,1.0), (1.0,1.0,1.0,0.0,1.0), (2.0,1.0,1.0,1.0,0.0))
-	  val basis = generate_basis(A)
-	  val expected_basis = DenseMatrix((-1,1),(2, -1),(-1, -1),(1,0),(0,1))
-	  basis should be expected_basis
+	  val basis = GenerateBasis(A)
+	  val ExpectedBasis = DenseMatrix((-1,1),(2, -1),(-1, -1),(1,0),(0,1))
+	  basis should be ExpectedBasis
 	}
 }
 
-
-class orthonormalize_spec extends FlatSpec with Matchers {
+class OrthoSpec extends FlatSpec with Matchers {
 	behavior of "orthonormalize"
 	it should "take in a matrix of size (3,3); the basis" in {
 	  val A = DenseMatrix((1,2,5), (1,1,1), (1,0,3))
-	  val basis_orthonormal = orthonormalize(A)
-	  val expected_basis_orthonormal = DenseMatrix((1/sqrt(3), 1/sqrt(2), 1/sqrt(6)), (1/sqrt(3), 0, -sqrt(2)/sqrt(3)), (1/sqrt(3), 1/sqrt(2), 1/sqrt(6)))
-	  basis should be expected_basis
+	  val BasisOrthonormal = Ortho(A)
+	  val ExpectedBasisOrthonormal = DenseMatrix((1/sqrt(3), 1/sqrt(2), 1/sqrt(6)), (1/sqrt(3), 0, -sqrt(2)/sqrt(3)), (1/sqrt(3), 1/sqrt(2), 1/sqrt(6)))
+	  BasisOrthonormal should be ExpectedBasisOrthonormal
 	}
 }
 
-class get_endpoints_spec extends FlatSpec with Matchers {
-	behavior of "get_endpoints"
+class GetEndpointsSpec extends FlatSpec with Matchers {
+	behavior of "GetEndpoints"
 	it should "take in two vectors; a point (3) and a direction (3)" in {
 	  val p = DenseVector(0,0.5,0.5)
 	  val q = DenseVector(2,1,2)
-	  val endpoints = get_endpoints(p,q)
-	  val first_endpoint  = endpoints._1
-	  val second_endpoint = endpoints._2
-	  val exp_first_endpoint = DenseVector(0,0.5,0.5)
-	  val exp_second_endpoint = DenseVector(0.5, 0.75, 1)
-	  first_endpoint should be expected_endpoints
-      second_endpoint
+	  val endpoints = GetEndpoints(p,q)
+	  val FirstEndpoint  = endpoints._1
+	  val SecondEndpoint = endpoints._2
+	  val ExpectedFirstPoint = DenseVector(0,0.5,0.5)
+	  val ExpectedSecondPoint = DenseVector(0.5, 0.75, 1)
+	  FirstEndpoint should be ExpectedFirstPoint
+      SecondEndpoint should be ExpectedSecondPoint
 	}
 }
 
