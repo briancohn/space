2286671ec127c2487963485458f50584b6b061e6
Brian Cohn <brian_cohn@pitzer.edu>
2015-04-14 21:11:40 -0700

    working beautified activation raw distributions

--- src/R/fixed_alpha_muscle_histograms.R
+++ src/R/fixed_alpha_muscle_histograms.R
@@ -12,19 +12,20 @@ fixed_alpha_muscle_histograms <- function (filename, fixedl_alpha_val, columnNam
 	pdf('src/latex/figs/raw_histograms.pdf')
 	par(mfrow=c(4,2))
 	for (i in 1:num_muscles) {
-		breaks_vec = seq(0,1.0,length.out=50)
+		breaks_vec = seq(0,1.0,length.out=100)
 		hist(db[,i], xlim=c(0.0,1.0), main=colnames(db)[i], breaks=breaks_vec, xlab='Activation',
 								freq=TRUE,
 								bty="n",
+								font.main = 1,
 								col="#A13F25",
 								lty="blank"
 									)
 		# Plot the observed bounds
-		abline(v=max(db[,i]),col="orange", lty=2)
-		abline(v=min(db[,i]),col="orange", lty=2)
 		if (class(abline_vals) != "NULL") {
 			abline(v=abline_vals[i], col="darkgrey", lwd=2)
 		}
+		abline(v=max(db[,i]),col="darkorange",lwd=1.5, lty=2)
+		abline(v=min(db[,i]),col="darkorange",lwd=1.5, lty=2)
 	}
 
 	dev.off()
--- src/R/main.R
+++ src/R/main.R
@@ -12,12 +12,32 @@ csvlist = c(
 "Y_alphaProgression1429065076877.csv",
 "Z_alphaProgression1429065090943.csv" 
 )
-columnNames <- c("fdp", "fds", "eip", "edc", "lum", "di", "pi", "fx", "fy" , "fz", "tx", "alpha", "l1", "l2", "l3", "l1w", "l2w", "l3w")
+# columnNames <- c("fdp", "fds", "eip", "edc", "lum", "di", "pi", "fx", "fy" , "fz", "tx", "alpha", "l1", "l2", "l3", "l1w", "l2w", "l3w")
+
+columnNames <- c(
+	"flexor digitorum profundus",
+	"flexor digitorum superficialis",
+	"extensor indicis proprius",
+	"extensor digitorum communis",
+	"lumbrical",
+	"dorsal interosseous",
+	"palmar interosseous",
+	"fx",
+	"fy",
+	"fz",
+	"tx",
+	"alpha",
+	"l1",
+	"l2",
+	"l3",
+	"l1w",
+	"l2w",
+	"l3w"
+  )
 num_muscles <- 7
 alpha_col <- match("alpha", columnNames)
 csvlist_cutoff<- cutoff_dotCSV(csvlist)
 print(paste(csvlist[3], "is being generated as raw_histograms.pdf"))
-max_alpha_solutions <- c(0.5,0.5,0.5,0.5,0.5,0.5,0.5)
 max_alpha_solutions <- get_max_alpha_solutions(paste0('output/',csvlist[4]), num_muscles=num_muscles, alpha_col)
 fixed_alpha_muscle_histograms(csvlist[4], 0.5, columnNames, abline_vals=max_alpha_solutions)
 activation_progressions(csvlist_cutoff, columnNames=columnNames)
