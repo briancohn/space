c3cf594c6977d8a614d599a5db644c99c6fedd33
bcohn12 <bcohn12@gmail.com>
2016-02-24 15:00:39 -0800

    working subsampling algorithm

--- src/R/subsample_from_hit_run_walk.R
+++ src/R/subsample_from_hit_run_walk.R
@@ -3,7 +3,17 @@ subsample_every_nth_row <- function(dataframe, interval){
   dataframe[seq(interval, length(dataframe[,1]), interval),]
 }
 
-fulldata <- read.csv("output/toy_example_recursive1.78666N_positive1456345658817.csv",header=FALSE)
-subsampled_data <- subsample_every_nth_row(fulldata, 100)
+feasible_x_for_null       <- read.csv("output/toy_example_recursive1.78666N_positive1456345658817.csv",header=FALSE)
+feasible_x_for_palmar     <- read.csv("output/toy_example_recursive0.013339999999999907N_positive1456345537716.csv", header=FALSE)
+feasible_x_for_off_palmar <- read.csv("output/toy_example_recursive5.3332999999999995N_positive1456345661011.csv", header=FALSE)
+
+subsampled_null   <- subsample_every_nth_row(feasible_x_for_null, 100)
+subsampled_palmar <- subsample_every_nth_row(feasible_x_for_palmar,100)
+subsampled_off_palmar <- subsample_every_nth_row(feasible_x_for_off_palmar,100)
+
+null_and_palmar <- merge(subsampled_null, subsampled_palmar)
+subsampled_data <- merge(null_and_palmar, subsampled_off_palmar)
+
+
 write.csv(subsampled_data, "output/cadaver_ready_data.csv", row.names=FALSE)
 
