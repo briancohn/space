68e539455aaa6ef061e5526b347352054d13b4f4
bcohn12 <bcohn12@gmail.com>
2016-02-24 13:06:06 -0800

    working subsampler when manually adding csv output file from scala

---
+++ src/R/subsample_from_hit_run_walk.R
@@ -0,0 +1,7 @@
+library(rgl)
+subsample_every_nth_row <- function(dataframe, interval){
+  dataframe[seq(100, length(dataframe[,1]), 100),]
+}
+
+hit_run_total_points[seq(100, length(hit_run_total_points[,1]), 100),]
+
