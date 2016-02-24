bcebf65c35e9636542b97f0a7c63ac58e8616554
bcohn12 <bcohn12@gmail.com>
2016-02-24 13:15:01 -0800

    added save functionality to csv

--- src/R/subsample_from_hit_run_walk.R
+++ src/R/subsample_from_hit_run_walk.R
@@ -1,7 +1,9 @@
 library(rgl)
 subsample_every_nth_row <- function(dataframe, interval){
-  dataframe[seq(100, length(dataframe[,1]), 100),]
+  dataframe[seq(interval, length(dataframe[,1]), interval),]
 }
 
-hit_run_total_points[seq(100, length(hit_run_total_points[,1]), 100),]
+fulldata <- read.csv("output/toy_example_recursive1.78666N_positive1456345658817.csv",header=FALSE)
+subsampled_data <- subsample_every_nth_row(fulldata, 100)
+write.csv(subsampled_data, "output/cadaver_ready_data.csv", row.names=FALSE)
 
