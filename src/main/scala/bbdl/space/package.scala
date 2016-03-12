a68b6fea9e9de45e43a5202ec617eab676c05e52
bcohn12 <bcohn12@gmail.com>
2016-03-11 16:28:01 -0800

    removed cadaver excess code. See #49

--- src/main/scala/bbdl/space/package.scala
+++ src/main/scala/bbdl/space/package.scala
@@ -73,35 +73,6 @@ def hit_and_run_repetitions(H: DenseMatrix[Double], forcevector: DenseVector[Dou
   }
 
 
-def cadaver_experiment_H_hit_and_run(num: Int, force_vector: DenseVector[Double]) {
-    import bbdl.space._
-    import breeze.linalg._
-    import breeze.numerics._
-    import breeze.stats._
-    val H_inverted = DenseMatrix(
-      (-0.014591639144483588,-0.022978454210103132,-0.025904423996600777,-0.00074314929316360008,0.02905683708515799,0.039418250858721381),
-      (-0.72695803910812851,-0.89355784559901341,1.3251944882192976,-0.25406166990994083,-1.6679512081270493,-0.66884960758196821),
-      (0.0098418024211056644,0.014705693921162236,-0.012187842899435046,0.0015156840441345504,0.011094297953993153,0.025993177318353322),
-      (-0.011308725178954909,-0.026337791625399426,-0.01759803436853305,-0.0012342879666309076,0.02759549461357709,-0.024314133219550324),
-      (-0.052470830930441033,-0.062128293533437497,0.10031391656256949,0.0035509814140534685,-0.13371084553191795,-0.03093669541061039),
-      (-0.035172533320395709,-0.038834705446617303,0.12056741892517414,-0.0083866339168305482,-0.17108381572684578,-0.034351131281275503),
-      (-0.03434527032650516,-0.049846961079497459,0.13386517901452974,-0.0049010529463190044,-0.15254523824400101,-0.0076352171312698827)
-    )
-    val H = H_inverted.t
-    csvwrite(new java.io.File("quickHtransposed.csv"),H)
-    //val forcevector_scaled_down = forcevector*1.0
-    val StartingPoint = GenStartingPoint(H,force_vector)
-    println("Starting point is " +StartingPoint)
-    val feasible_activations = hit_and_run_recursive_acc_with_intermediate_steps(Ortho(Basis(H)).toDenseMatrix, DenseMatrix.zeros[Double](1,H.cols),num,StartingPoint)
-    println(
-      "id174892" + " feasible activations are /n" + feasible_activations
-    )
-    val FileName = Output.TimestampCSVName("output/" + "toy_example_recursive" + force_vector(0) +"N_positive").toString()
-    val MyFile = new java.io.File(FileName)
-    csvwrite(MyFile, feasible_activations)
-    println("Saved" + FileName)
-  }
-
 val toy_arm_example_H = DenseMatrix(
   (10.0/3.0, -53.0/15.0, 2.0)
 )
@@ -129,8 +100,7 @@ def toy_example_recursive(num: Int, force_vector: DenseVector[Double]) {
   }
 
 
-
-  //TODO important! this does not subsample,so that needs to be addressed on a higher level
+//TODO important! this does not subsample,so that needs to be addressed on a higher level
 @tailrec  
 def hit_and_run_recursive_acc_with_intermediate_steps(OrthonormalBasis: DenseMatrix[Double],matrix_so_far: DenseMatrix[Double], iterations_remaining:Int, CurrentPoint: DenseVector[Double]): DenseMatrix[Double] = {
   def we_have_done_enough_samples(n: Int): Boolean = {n == 0}
@@ -154,6 +124,9 @@ def hit_and_run_recursive_acc_with_intermediate_steps(OrthonormalBasis: DenseMat
 // @param iterations_remaining is the number of jumps to make before returning the new point. We recommend 100 because experimental evidence supports this for at least 7 variables.
 @tailrec  
 def HR_uar_point(OrthonormalBasis: DenseMatrix[Double], iterations_remaining:Int, CurrentPoint: DenseVector[Double], per_uar_point_random_seed: scala.util.Random): DenseVector[Double] = {
+  println("Starting new HR Jump Pattern. Random input is: " + per_uar_point_random_seed)
+  println("one randomval from this one is" + new scala.util.Random(per_uar_point_random_seed.nextInt).nextFloat)
+  println("one randomval from this one is" + new scala.util.Random(per_uar_point_random_seed.nextInt).nextFloat)
   def we_have_done_enough_samples(n: Int): Boolean = {n == 0}
 
   if (we_have_done_enough_samples(iterations_remaining)) {
@@ -163,7 +136,7 @@ def HR_uar_point(OrthonormalBasis: DenseMatrix[Double], iterations_remaining:Int
   else {
     //gen new point
     println("Iterations until UAR remaining: " + iterations_remaining)
-    val NewPoint = HitAndRun(OrthonormalBasis, CurrentPoint, new scala.util.Random(iterations_remaining)) //here I use the iterations as the seed
+    val NewPoint = HitAndRun(OrthonormalBasis, CurrentPoint, new scala.util.Random(per_uar_point_random_seed.nextInt)) //here I use the iterations as the seed
     //recurse now with the new point as the new seed
     HR_uar_point(OrthonormalBasis, iterations_remaining - 1, NewPoint, new scala.util.Random(iterations_remaining))
   }
