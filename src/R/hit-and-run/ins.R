624c34950e8837d2873ee007d70fe63d3b644d4f
bcohn12 <bcohn12@gmail.com>
2016-11-02 09:51:39 -0700

    rm sampledata, turn ins.r into mainfunction. See #55

--- src/R/hit-and-run/ins.R
+++ src/R/hit-and-run/ins.R
@@ -8,12 +8,17 @@ data= c(-0.08941, -0.0447, 0.2087, -0.2138, -0.009249, 0.1421, 0.03669,
         0.06472, 0.001953, 0.0568, 0.2067, -0.1518, 0.2919, -0.1518,
         0.003081, -0.002352, 0.0001578, -0.000685, -0.0001649, -0.0004483, -0.0001649)
 JR = matrix(data,4,7,byrow = TRUE)
-
-# fmax
+# fmax is the maximal force output
 fmax=c(123, 219, 124.8, 129.6, 23.52, 21.6, 91.74)
 
-# produce all gifs
-files <- list.files(path="~/dev/github/space/output/", pattern="*.csv", full.names=T, recursive=FALSE)
-lapply(files, function(file) {
-  paracoord_plot(file, fmax, 9, 0.7, outputdir="~/dev/github/space/src/R/hit-and-run/plots/")
-})
+main <- function(path="~/Documents/GitHub/bcohn12/space/output/", output_directory="~/Documents/GitHub/bcohn12/space/src/R/hit-and-run/plots/") {
+	# produce all image frame 
+	files <- list.files(path, pattern="*.csv", full.names=T, recursive=FALSE)
+	lapply(files, function(file) {
+	  paracoord_plot(file, fmax, 9, 0.7, output_directory)
+	})
+}
+
+main()
+
+#afterwards, take the pictures in the folder and make a GIF or movie.
